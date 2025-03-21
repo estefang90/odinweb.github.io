84550c4f798bf2f2c154538ae79cf8e210befbc6
estefang90 <36801483+estefang90@users.noreply.github.com>
2025-03-21 14:28:17 -0500

    cambios send

--- send.php
+++ send.php
@@ -1,12 +1,12 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     // Recoge los datos del formulario
     $name = $_POST['user_name'];
     $email = $_POST['user_email'];
     $mensaje = $_POST['user_message'];
     
     // Destinatario del correo electrónico
-    $para = 'estefangz@gmail.com';
+    $para = 'facturacionweb@hotmail.com';
     
     // Asunto del correo electrónico
     $asunto = 'Nuevo mensaje de contacto';
@@ -17,15 +17,16 @@
     $cuerpoMensaje .= "Mensaje:\n$mensaje";
     
     // Cabeceras del correo electrónico
-    $headers = "From: $nombre <$email>" . "\r\n" .
+    $headers = "From: $name <$email>" . "\r\n" .
                  "Reply-To: $email" . "\r\n" .
                  "X-Mailer: PHP/" . phpversion();
     
     // Envía el correo electrónico
-    mail($para, $asunto, $cuerpoMensaje, $headers);
-    
-    header("Location: index.html");
-    exit();
+    if (mail($para, $asunto, $cuerpoMensaje, $headers)) {
+        header("Location: index.html");
+    } else {
+        echo 'Error al enviar el mensaje.';
     }
+
 //
 ?>
